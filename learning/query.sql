81c7c80996b5ae626b1ce7789d62d007b0332d65
md-samrat <md.samrat61790@gmail.com>
2026-07-03 20:15:53 +0600

    more query

--- learning/query.sql
+++ learning/query.sql
@@ -18,3 +18,44 @@ WHERE age > 20;
 
 DELETE FROM person
 WHERE id = 109;
+
+
+
+-- DISTINCT
+SELECT DISTINCT age FROM person;
+
+
+-- AND 
+
+SELECT name,email,dept FROM person WHERE age > 20 AND dept = 'IT';
+
+
+-- OR 
+
+SELECT * FROM person WHERE age > 20 OR dept='marketing';
+
+
+-- LIMIT 
+
+SELECT * FROM person LIMIT 4; 
+
+-- OFFSET
+SELECT * FROM person OFFSET 5;
+
+-- ORDER BY
+SELECT * FROM person OFFSET 5;
+
+-- ORDER BY age DESC;
+SELECT * FROM person ORDER BY age DESC;
+
+-- LIKE
+
+SELECT name from person WHERE name LIKE '%n';
+
+-- IN
+SELECT * FROM person
+WHERE id IN (100, 104, 108);
+
+-- BETWEEN
+SELECT * FROM person
+WHERE age BETWEEN 20 AND 30;
